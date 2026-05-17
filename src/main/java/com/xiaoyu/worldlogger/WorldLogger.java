3545b5382769c930f81485b9f3a8319df124f527
create-xiaoyu <abc18873060306@qq.com>
2026-05-17 16:41:07 +0800

    添加初始化表、添加玩家信息相关表

--- src/main/java/com/xiaoyu/worldlogger/WorldLogger.java
+++ src/main/java/com/xiaoyu/worldlogger/WorldLogger.java
@@ -1,14 +1,43 @@
 package com.xiaoyu.worldlogger;
 
+import com.xiaoyu.worldlogger.mysql.DataBase;
+import com.xiaoyu.worldlogger.mysql.InitMySQL;
+import com.xiaoyu.worldlogger.mysql.MySQLExecutorService;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartedEvent;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 import net.neoforged.fml.common.Mod;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @Mod(WorldLogger.MODID)
 public class WorldLogger {
     public static final String MODID = "worldlogger";
+    private static final Logger LOGGER = LogUtils.getLogger();
 
-    public static final Logger LOGGER = LogUtils.getLogger();
+    public WorldLogger(ModContainer container, IEventBus modBus) {
+        NeoForge.EVENT_BUS.addListener(WorldLogger::onServerStarted);
 
+        container.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    public static void onServerStarted(ServerStartedEvent event) {
+        if (!(FMLEnvironment.getDist().isDedicatedServer())) return;
+
+        InitMySQL.InitHikari();
+        MySQLExecutorService.init();
+
+        try (Connection mySQLConnection = InitMySQL.getMySQLConnection()) {
+            DataBase.InitDataBase(mySQLConnection);
+        } catch (SQLException e) {
+            LOGGER.error("Failed to connect to MySQL server!", e);
+        }
+    }
 }
---
+++ src/main/java/com/xiaoyu/worldlogger/mysql/DataBase.java
@@ -0,0 +1,72 @@
+package com.xiaoyu.worldlogger.mysql;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataBase {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static void InitDataBase(Connection mysqlConnection) {
+        try (Statement statement = mysqlConnection.createStatement()) {
+            statement.execute(
+                    """
+                        CREATE TABLE IF NOT EXISTS PLAYER_BASE_INFO(
+                            player_uuid VARCHAR(36) PRIMARY KEY,
+                            player_name VARCHAR(16) NOT NULL,
+                            player_game_time BIGINT
+                        )
+                        """
+            );
+            statement.execute(
+                    """
+                        CREATE TABLE IF NOT EXISTS PLAYER_SESSION_INFO(
+                            data_id INT AUTO_INCREMENT PRIMARY KEY,
+                            player_uuid VARCHAR(36) NOT NULL,
+                            player_name VARCHAR(16) NOT NULL,
+                            player_login_time DATETIME,
+                            player_login_pos VARCHAR(64),
+                            player_login_world VARCHAR(64),
+                            player_logout_time DATETIME,
+                            player_logout_pos VARCHAR(64),
+                            player_logout_world VARCHAR(64)
+                        )
+                        """
+            );
+            statement.execute(
+                    """
+                        CREATE TABLE IF NOT EXISTS PLAYER_DEATH_INFO(
+                            data_id INT AUTO_INCREMENT PRIMARY KEY,
+                            player_uuid VARCHAR(36) NOT NULL,
+                            player_name VARCHAR(16) NOT NULL,
+                            player_death_time DATETIME,
+                            player_death_pos VARCHAR(64),
+                            player_death_world VARCHAR(64),
+                            player_death_source TEXT,
+                            player_death_lost_xp DOUBLE,
+                            player_death_lost_item TEXT
+                        )
+                        """
+            );
+            statement.execute(
+                    """
+                        CREATE TABLE IF NOT EXISTS PLAYER_XP_INFO(
+                            data_id INT AUTO_INCREMENT PRIMARY KEY,
+                            player_uuid VARCHAR(36) NOT NULL,
+                            player_name VARCHAR(16) NOT NULL,
+                            player_xp_add_time DATETIME,
+                            player_xp_add_count DOUBLE,
+                            player_xp_add_pos VARCHAR(64),
+                            player_xp_add_world VARCHAR(64),
+                            player_xp_count DOUBLE
+                        )
+                        """
+            );
+        } catch (SQLException e) {
+            LOGGER.error("Error while initializing DataBase!", e);
+        }
+    }
+}
