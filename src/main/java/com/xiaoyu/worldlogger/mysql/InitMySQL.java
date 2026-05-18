dd19a1659cd483c6c81061c13df7faba36cd28ac
create-xiaoyu <abc18873060306@qq.com>
2026-05-18 08:48:29 +0800

    删除冗余判断

--- src/main/java/com/xiaoyu/worldlogger/mysql/InitMySQL.java
+++ src/main/java/com/xiaoyu/worldlogger/mysql/InitMySQL.java
@@ -12,7 +12,6 @@ import java.sql.SQLException;
 public class InitMySQL {
     private static final Logger LOGGER = LogUtils.getLogger();
     private static HikariDataSource DataSource;
-    private static boolean available = false;
 
     public static void InitHikari() {
         try {
@@ -35,7 +34,6 @@ public class InitMySQL {
 
             DataSource = new HikariDataSource(hikariConfig);
 
-            available = true;
             LOGGER.info("Hikari DataSource initialized successfully.");
         } catch (Exception e) {
             LOGGER.error("Hikari DataSource initialization failed.", e);
@@ -43,7 +41,7 @@ public class InitMySQL {
     }
 
     public static Connection getMySQLConnection() throws SQLException {
-        if (DataSource == null || !(available)) {
+        if (DataSource == null) {
             throw new IllegalStateException("HikariCP not initialized");
         }
         return DataSource.getConnection();
@@ -52,11 +50,6 @@ public class InitMySQL {
     public static void closeHikari() {
         if (DataSource != null && !(DataSource.isClosed())) {
             DataSource.close();
-            available = false;
         }
-    }
-
-    public static boolean isAvailable() {
-        return available;
     }
 }
