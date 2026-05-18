aa852853a250b6cbda015d6debdb6b857d6471b5
create-xiaoyu <abc18873060306@qq.com>
2026-05-18 08:52:11 +0800

    主类注册相关事件

--- src/main/java/com/xiaoyu/worldlogger/WorldLogger.java
+++ src/main/java/com/xiaoyu/worldlogger/WorldLogger.java
@@ -1,5 +1,6 @@
 package com.xiaoyu.worldlogger;
 
+import com.xiaoyu.worldlogger.event.PlayerEvent.PlayerLoggedInEvent.WriteTable.WriteTablePlayer;
 import com.xiaoyu.worldlogger.mysql.DataBase;
 import com.xiaoyu.worldlogger.mysql.InitMySQL;
 import com.xiaoyu.worldlogger.mysql.MySQLExecutorService;
@@ -9,6 +10,7 @@ import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStartedEvent;
+import net.neoforged.neoforge.event.server.ServerStoppedEvent;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -23,21 +25,29 @@ public class WorldLogger {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public WorldLogger(ModContainer container, IEventBus modBus) {
-        NeoForge.EVENT_BUS.addListener(WorldLogger::onServerStarted);
+        if (FMLEnvironment.getDist().isDedicatedServer()) {
+            NeoForge.EVENT_BUS.addListener(WorldLogger::onServerStarted);
+            NeoForge.EVENT_BUS.addListener(WorldLogger::onServerStopped);
+
+            NeoForge.EVENT_BUS.register(WriteTablePlayer.class);
+        }
 
         container.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     public static void onServerStarted(ServerStartedEvent event) {
-        if (!(FMLEnvironment.getDist().isDedicatedServer())) return;
-
         InitMySQL.InitHikari();
         MySQLExecutorService.init();
 
         try (Connection mySQLConnection = InitMySQL.getMySQLConnection()) {
-            DataBase.InitDataBase(mySQLConnection);
+            DataBase.InitDataBaseTable(mySQLConnection);
         } catch (SQLException e) {
             LOGGER.error("Failed to connect to MySQL server!", e);
         }
+    }
+
+    public static void onServerStopped(ServerStoppedEvent event) {
+        MySQLExecutorService.shutdown();
+        InitMySQL.closeHikari();
     }
 }
