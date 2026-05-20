81c5beb8873b34852f131a3da263f79a29ab1663
create-xiaoyu <abc18873060306@qq.com>
2026-05-21 01:38:01 +0800

    将工具类方法移动到合理的类中

--- src/main/java/com/xiaoyu/worldlogger/utils/StringData.java
+++ src/main/java/com/xiaoyu/worldlogger/utils/StringData.java
@@ -1,9 +1,43 @@
 package com.xiaoyu.worldlogger.utils;
 
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class StringData {
     public static String getLevelName(Level level) {
         return String.format("[%s], %S", level.getDescription().getString(), level.dimension().identifier());
+    }
+
+    public static String getPos(Entity entity) {
+        return String.format("[X: %d, Y: %d, Z: %d]", entity.getBlockX(), entity.getBlockY(), entity.getBlockZ());
+    }
+
+    public static String getPos(Player player) {
+        return String.format("[X: %d, Y: %d, Z: %d]", player.blockPosition().getX(), player.blockPosition().getY(), player.blockPosition().getZ());
+    }
+
+    public static long getGameTime(ServerPlayer player) {
+        return player.getStats().getValue(Stats.CUSTOM.get(Stats.PLAY_TIME)) / 20;
+    }
+
+    public static String getTime() {
+        LocalDateTime nowTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return nowTime.format(formatter);
+    }
+
+    public static String getPlayerIP (ServerPlayer serverPlayer) {
+        SocketAddress socketAddress = serverPlayer.connection.getRemoteAddress();
+        if (!(socketAddress instanceof InetSocketAddress inetSocketAddress)) return null;
+
+        return inetSocketAddress.getAddress().getHostAddress();
     }
 }
